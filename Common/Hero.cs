272dd801f4344a43284a7dbc192e2eb41b2a8f4f
almasmendybayev <almasmendybayev@tengizchevroil.com>
2023-01-20 18:04:11 +0600

    base.Method(), override

---
+++ Common/Hero.cs
@@ -0,0 +1,29 @@
+﻿namespace sandbox.Common
+{
+    public class Hero
+    {
+        public string Name { get; set; }
+
+        public virtual void Kick()
+        {
+            Console.WriteLine("Bang-bang");
+        }
+    }
+    public class AllMight : Hero
+    {
+        public override void Kick()
+        {
+            Console.WriteLine("One 2 All");
+            //base.Kick();
+        }
+    }
+
+    public class Deku : AllMight
+    {
+        public override void Kick()
+        {
+            base.Kick();
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -3,24 +3,8 @@
 var dog = new Dog();
 var cat = new Cat();
 
-//dog.Jump();
-//cat.Jump();
+Hero hero = new Hero();
+hero.Kick();
 
-//dog.Run();
-//cat.Run();
-
-//Jump from Dog class
-//Jump from Cat class
-//run
-//run from Cat
-
-var c = new Car("car ctor");
-Console.WriteLine();
-
-var n = new Nissan("nissan ctor");
-Console.WriteLine();
-
-Stellantis s = new Stellantis("Stellantis");
-Console.WriteLine();
-
-Fiat f = new Fiat("Fiat");
+Hero heroNumber1 = new AllMight();
+heroNumber1.Kick();
