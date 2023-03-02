aa1c81e33e0d9d4af73a5a2216f24e9445cfb10c
almasmendybayev <almas_mendybayev@epam.com>
2023-03-02 16:45:19 +0600

    static

--- Program.cs
+++ Program.cs
@@ -2,10 +2,15 @@
 int _value;
 
 
-InModifierTest(in age);
-RefModifierTest(ref age);
-OutModifierTest(out _value);
+//InModifierTest(in age);
+//RefModifierTest(ref age);
+//OutModifierTest(out _value);
+Test test = new Test();
+test.SetValue(10);
+int value = test.GetValue();
 
+Console.WriteLine(test.Age);
+Console.WriteLine(value);
 void InModifierTest(in int age) {
     //age += 1;
     Console.WriteLine($"{age}");
@@ -19,4 +24,24 @@ void OutModifierTest(out int _value) {
     _value = 0;
     _value += 10;
     Console.WriteLine($"{_value}");
+}
+
+public class Test {
+    private readonly int _age;
+    private static int _value;
+
+    public Test() {
+        _age = 10;
+    }
+
+    public int Age {
+        get => _age;
+    }
+
+    public void SetValue(int value) {
+        _value = value;
+    }
+    public int GetValue() {
+        return _value;
+    }
 }
