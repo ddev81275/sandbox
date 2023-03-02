de2028cffa109f4cc153c3159cc5bb0404b28e6d
almasmendybayev <almas_mendybayev@epam.com>
2023-03-02 16:11:26 +0600

    ref, out, in modifier

--- Program.cs
+++ Program.cs
@@ -1,5 +1,22 @@
 ﻿int age = 55;
-PrintAge(in age);
-void PrintAge(in int age) {
+int _value;
+
+
+InModifierTest(in age);
+RefModifierTest(ref age);
+OutModifierTest(out _value);
+
+void InModifierTest(in int age) {
+    //age += 1;
     Console.WriteLine($"{age}");
+}
+
+void RefModifierTest(ref int age) {
+    Console.WriteLine("ref");
+}
+
+void OutModifierTest(out int _value) {
+    _value = 0;
+    _value += 10;
+    Console.WriteLine($"{_value}");
 }
