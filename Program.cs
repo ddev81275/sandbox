b3b27be641a875acb56370b2be241c9c711fe212
almasmendybayev <almas_mendybayev@epam.com>
2023-03-03 16:07:22 +0600

    async, await c#

--- Program.cs
+++ Program.cs
@@ -1,51 +1,13 @@
-﻿int age = 55;
-int _value;
+﻿var values = new int[] { 1, 2, 3, 4, 5 };
 
-
-//InModifierTest(in age);
-//RefModifierTest(ref age);
-//OutModifierTest(out _value);
-Test test = new Test();
-test.SetValue(10);
-int value = test.GetValue();
-int year = Test.year;
-
-Console.WriteLine(test.Age);
-Console.WriteLine(value);
-Console.WriteLine(year);
-
-void InModifierTest(in int age) {
-    //age += 1;
-    Console.WriteLine($"{age}");
-}
-
-void RefModifierTest(ref int age) {
-    Console.WriteLine("ref");
-}
-
-void OutModifierTest(out int _value) {
-    _value = 0;
-    _value += 10;
-    Console.WriteLine($"{_value}");
-}
-
-class Test {
-    private readonly int _age;
-    // Static variable that must be initialized at run time.
-    private static int _value;
-    public static int year;
-    static Test() {
-        year = DateTime.Now.Year;
+Parallel.ForEach(values, value => {
+    if (value % 2 == 0) {
+        Console.WriteLine(value);
     }
-
-    public int Age {
-        get => _age;
+});
+Console.WriteLine("---------------------");
+Parallel.ForEach(values, value => {
+    if (value % 2 == 1) {
+        Console.WriteLine(value);
     }
-
-    public void SetValue(int value) {
-        _value = value;
-    }
-    public int GetValue() {
-        return _value;
-    }
-}
+});
